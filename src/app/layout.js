d91c3ec8362f2ad0cf32aa48e1188b4e7a84f16d
Himanshu Kumar <himanshukumar077@gmail.com>
2024-02-07 09:51:18 +0530

    in progress

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import { Box } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
