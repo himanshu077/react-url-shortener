17524b054b9926e9f34aa65d393d8b8c6a6ccc72
Himanshu Kumar <himanshukumar077@gmail.com>
2024-02-26 13:32:52 +0530

    Fixed Bugs related to theme button and tab

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       maxWidth: {
-        "8xl": "88.8rem",
+        "8xl": "79.176vw"
       },
       inset: {
         "1/2": "57.6%",
